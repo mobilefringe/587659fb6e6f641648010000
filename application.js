d77947504c232c5f27cf3310b4122f5a19809b2e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-01-13 16:29:32 -0500

    Primaris Template 1.0  /application.js

--- application.js
+++ application.js
@@ -131,12 +131,12 @@ function renderFeatureItems(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
-function renderGeneral(container, template, collection){
+function renderGeneral(container, template, prop_details){
     var item_list = [];
     var item_rendered = [];
     var template_html = $(template).html();
     Mustache.parse(template_html); 
-    $.each( collection , function( key, val ) {
+    $.each(prop_details , function( key, val ) {
         var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
     });
