5f26aa161f6e4a635d3fc101eecc5a89abc1ea5a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-01-16 12:58:39 -0500

    Primaris Template 1.0  /application.js

--- application.js
+++ application.js
@@ -210,13 +210,13 @@ function renderHours(container, template, collection, type){
     Mustache.parse(template_html);   // optional, speeds up future uses
     if (type == "reg_hours") {
         $.each( collection , function( key, val ) {
-            var current_day = getTodaysHours().day_of_week;
-            console.log(current_day);
-            var day_of_week = val.day_of_week;
-            console.log(day_of_week);
-            if(current_day === day_of_week) {
-                $('.drop-down-row').addClass('.drop-down-row-today');
-            }
+            // var current_day = getTodaysHours().day_of_week;
+            // console.log(current_day);
+            // var day_of_week = val.day_of_week;
+            // console.log(day_of_week);
+            // if(current_day === day_of_week) {
+            //     $('.drop-down-row').addClass('.drop-down-row-today');
+            // }
             if (!val.store_id && val.is_holiday == false) {
                 switch(val.day_of_week) {
                     case 0:
